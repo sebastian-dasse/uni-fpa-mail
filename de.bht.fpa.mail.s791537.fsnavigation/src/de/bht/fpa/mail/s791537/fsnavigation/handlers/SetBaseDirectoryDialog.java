77a7d1cdeb499d3564547405687ff099830cc3de
sebastian-dasse <s50602@beuth-hochschule.de>
2013-11-15 15:36:38 +0100

    Add SetBaseDirectoryDialog.

---
+++ de.bht.fpa.mail.s791537.fsnavigation/src/de/bht/fpa/mail/s791537/fsnavigation/handlers/SetBaseDirectoryDialog.java
@@ -0,0 +1,13 @@
+package de.bht.fpa.mail.s791537.fsnavigation.handlers;
+
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.swt.widgets.Shell;
+
+public class SetBaseDirectoryDialog extends Dialog {
+
+  protected SetBaseDirectoryDialog(Shell parentShell) {
+    super(parentShell);
+    // TODO Auto-generated constructor stub
+  }
+
+}
