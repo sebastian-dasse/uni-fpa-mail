33f77219dd7abd7de4a58e54c401a52d05459cc9
sebastian-dasse <s50602@beuth-hochschule.de>
2014-01-09 22:33:14 +0100

    Clean up filter main.

--- de.bht.fpa.mail.s791537.filter/src/de/bht/fpa/mail/s791537/filter/Main.java
+++ de.bht.fpa.mail.s791537.filter/src/de/bht/fpa/mail/s791537/filter/Main.java
@@ -1,10 +1,6 @@
 package de.bht.fpa.mail.s791537.filter;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import de.bht.fpa.mail.s000000.common.filter.FilterOperator;
-import de.bht.fpa.mail.s000000.common.mail.model.Importance;
 import de.bht.fpa.mail.s000000.common.mail.model.Message;
 import de.bht.fpa.mail.s000000.common.mail.testdata.RandomTestDataProvider;
 import de.bht.fpa.mail.s000000.common.table.MessageValues;
@@ -19,90 +15,24 @@ public final class Main {
 
   public static void main(String[] args) {
     Iterable<Message> messages = new RandomTestDataProvider(NUMBER_OF_MESSAGES).getMessages();
-    // printAll(test0(messages), MessageValues.IMPORTANCE);
-    // printAll(test1(messages), MessageValues.IMPORTANCE,
-    // MessageValues.SUBJECT, MessageValues.SENDER,
-    // MessageValues.RECIPIENT);
-    // printAll(test2(messages));
-    // printAll(test2b(messages));
-    // printAll(test3(messages));
-    // printAll(test4(messages));
 
-    // printAll(test5(messages), MessageValues.READ);
-    // printAll(test6(messages), MessageValues.RECIPIENT);
-    // printAll(test7(messages), MessageValues.RECIPIENT);
-    printAll(test8(messages), MessageValues.SUBJECT);
-    printAll(test9(messages), MessageValues.SUBJECT);
+    printAll(test1(messages), MessageValues.READ);
+    printAll(test2(messages), MessageValues.SENDER, MessageValues.RECIPIENT);
+    printAll(test3(messages), MessageValues.SENDER, MessageValues.READ);
   }
 
-  public static Iterable<Message> test0(Iterable<Message> messages) {
-    // messages = new SenderFilter("karl",
-    // FilterOperator.STARTS_WITH).filter(messages);
-    // messages = new RecipientsFilter("karl stulle",
-    // FilterOperator.IS).filter(messages);
-    // messages = new SubjectFilter("f",
-    // FilterOperator.STARTS_WITH).filter(messages);
-    // messages = new TextFilter("something",
-    // FilterOperator.CONTAINS).filter(messages);
-    messages = new ImportanceFilter(Importance.HIGH).filter(messages);
-    // messages = new ReadFilter(true).filter(messages);
-    return messages;
-  }
-
-  // trivial intersection
-  public static Iterable<Message> test1(Iterable<Message> messages) {
-    messages = new SenderFilter("karl", FilterOperator.CONTAINS).filter(messages);
-    messages = new RecipientsFilter(".de", FilterOperator.ENDS_WITH).filter(messages);
-    messages = new SubjectFilter("All for nothing", FilterOperator.IS).filter(messages);
-    messages = new ImportanceFilter(Importance.LOW).filter(messages);
-    return messages;
-  }
-
-  public static Iterable<Message> test2(Iterable<Message> messages) {
-    return new IntersectionFilter(new SenderFilter("karl", FilterOperator.CONTAINS), new RecipientsFilter(".de",
-        FilterOperator.ENDS_WITH), new SubjectFilter("All for nothing", FilterOperator.IS), new ImportanceFilter(
-        Importance.LOW)).filter(messages);
-  }
-
-  public static Iterable<Message> test2b(Iterable<Message> messages) {
-    return new IntersectionFilter(new IntersectionFilter(new SenderFilter("karl", FilterOperator.CONTAINS),
-        new RecipientsFilter(".de", FilterOperator.ENDS_WITH)), new IntersectionFilter(new SubjectFilter(
-        "All for nothing", FilterOperator.IS), new ImportanceFilter(Importance.LOW))).filter(messages);
-  }
-
-  // trivial union
-  public static Iterable<Message> test3(Iterable<Message> messages) {
-    Set<Message> filteredMessages = new HashSet<Message>();
-    filteredMessages.addAll(new SenderFilter("karl stulle", FilterOperator.IS).filter(messages));
-    filteredMessages.addAll(new SubjectFilter("All for nothing", FilterOperator.IS).filter(messages));
-    return filteredMessages;
-  }
-
-  public static Iterable<Message> test4(Iterable<Message> messages) {
-    return new UnionFilter(new SenderFilter("karl stulle", FilterOperator.IS), new SubjectFilter("All for nothing",
-        FilterOperator.IS)).filter(messages);
-  }
-
-  private static Iterable<Message> test5(Iterable<Message> messages) {
+  private static Iterable<Message> test1(Iterable<Message> messages) {
     return new ReadFilter(false).filter(messages);
   }
 
-  private static Iterable<Message> test6(Iterable<Message> messages) {
+  private static Iterable<Message> test2(Iterable<Message> messages) {
     return new UnionFilter(new SenderFilter("karl", FilterOperator.CONTAINS), new RecipientsFilter("heidi stulle",
         FilterOperator.IS)).filter(messages);
   }
 
-  private static Iterable<Message> test7(Iterable<Message> messages) {
+  private static Iterable<Message> test3(Iterable<Message> messages) {
     return new IntersectionFilter(new SenderFilter("karl", FilterOperator.STARTS_WITH), new ReadFilter(true))
         .filter(messages);
-  }
-
-  private static Iterable<Message> test8(Iterable<Message> messages) {
-    return new IntersectionFilter(new SubjectFilter("free", FilterOperator.CONTAINS)).filter(messages);
-  }
-
-  private static Iterable<Message> test9(Iterable<Message> messages) {
-    return new UnionFilter(new SubjectFilter("free", FilterOperator.CONTAINS)).filter(messages);
   }
 
   @SuppressWarnings("unchecked")
@@ -111,7 +41,7 @@ public final class Main {
     int count = 0;
     for (Message message : messages) {
       for (IValue val : vals) {
-        System.out.print(((BaseValue<Message>) val).get(message) + "\t\t");
+        System.out.print(((BaseValue<Message>) val).get(message) + "   ***   ");
       }
       System.out.println(message);
       count++;
